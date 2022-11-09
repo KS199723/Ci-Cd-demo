62d28856e5f0d69d79c7a56ad565908b7dfcadd6
Ksingh33 <singh_kishan@network.lilly.com>
2022-11-09 19:26:33 +0530

    initial commit

---
+++ force-app/main/default/lwc/sum/sum.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+export function sum(x, y) {
+    return x + y;
+  }
+export default class Sum extends LightningElement {}
---
+++ force-app/main/default/lwc/wireTest/wireTest.js
@@ -0,0 +1,14 @@
+import { LightningElement,wire } from 'lwc';
+import getAccountList from '@salesforce/apex/accountList.getAccList';
+export default class WireTest extends LightningElement 
+{
+    @wire(getAccountList)
+    accs;
+
+    renderedCallback()
+    {
+      console.log('Data Loaded');
+      console.log(JSON.stringify(this.accs));
+    }
+}
+
